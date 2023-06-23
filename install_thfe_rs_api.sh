c6b8db0dfc73462629647b6a89c444c7ad1531fe
Levent Demir <levent.demir@zama.ai>
2023-06-23 16:20:30 +0200

    chore(build c api): call deterministic build of C api as in the workflow (ci test)

--- install_thfe_rs_api.sh
+++ install_thfe_rs_api.sh
@@ -4,6 +4,6 @@ git clone https://github.com/zama-ai/tfhe-rs.git
 git checkout 0.3.0-beta.0
 mkdir -p core/vm/lib
 cd tfhe-rs
-make build_c_api
+make build_c_api_experimental_deterministic_fft
 cp target/release/libtfhe.* ../core/vm/lib
 cp target/release/tfhe.h ../core/vm
